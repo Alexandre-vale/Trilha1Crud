17d845e1e5ff0865124ddc1902f1ebdc2073ea32
Alexandre-Vale <v-alexan@hotmail.com>
2021-04-23 15:23:11 -0300

    adicionado OpenID

--- cognito/OpenID.py
+++ cognito/OpenID.py
@@ -2,25 +2,19 @@ import requests as rq
 
 from flask import Flask, jsonify, request
 
+import jwt
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def print_url_from_get():
-    print(f'receiving get request')
     code = request.args.get('code')
+    secret = "aaa"
     response = rq.post('https://todoapi.auth.sa-east-1.amazoncognito.com/oauth2/token',{'Content-Type':'application/x-www-form-urlencoded', 'grant_type': 'authorization_code', 'client_id': 'vjmavfrop927pfnk9jn15h2ft',  'code': code, 'redirect_uri': 'http://localhost:8000/'})
     response = response.json()
-    print(response)
-    print(code)
-    return code
-
-@app.route("/redirect", methods=["GET"])
-def print_url_from_get_redirect():
-    print(f'receiving get request')
-    url = request.url
-    print(url)
-    return url
-
+    idtoken = response['id_token']
+    user = jwt.decode(idtoken, secret, algorithms=["RS256"], options={"verify_signature": False})
+    return user
 
 
 if __name__ == "__main__":
