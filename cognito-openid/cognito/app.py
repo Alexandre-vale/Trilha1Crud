4d08afe9bd3eb36c0c1eb05137ca892cb84ac3cd
Victor Pereira <victor.pereira@kumulus.com.br>
2021-04-23 15:53:10 -0300

    Lambda integrated with cognito

---
+++ cognito-openid/cognito/app.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+
+def lambda_handler(event, context):
+
+    code = event["queryStringParameters"]["code"]
+    
+    secret = "aaa"
+    response = requests.post(
+        "https://todoapi.auth.sa-east-1.amazoncognito.com/oauth2/token",
+        {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "grant_type": "authorization_code",
+            "client_id": "vjmavfrop927pfnk9jn15h2ft",
+            "code": code,
+            "redirect_uri": "http://localhost:3000/cognito/",
+        },
+    )
+
+    response = response.json()
+    idtoken = response["id_token"]
+    user = jwt.decode(
+        idtoken, secret, algorithms=["RS256"], options={"verify_signature": False}
+    )
+    return user
+
+    return {"statusCode": 200, "body": user}
