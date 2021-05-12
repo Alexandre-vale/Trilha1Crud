79ae42e97220e67d9e0a6fdf11f76b8972e169ab
Alexandre-Vale <v-alexan@hotmail.com>
2021-05-12 19:22:06 -0300

    adicionado novo filtro

--- ktask/crudapi/filters.py
+++ ktask/crudapi/filters.py
@@ -30,8 +30,36 @@ def lambda_handler(event, context):
     if filters[path] == "todolist":
         queryset = ToDo.objects(todolist=query_filter).all()
 
-    elif filters[path] == "owner":
-        queryset = ToDo.objects(owner=query_filter).all()
+    # elif filters[path] == "owner": Aqui esta o anterior, caso tentar fazer um novo por minha parte falhe
+    #     queryset = ToDo.objects(owner=query_filter).all()
+
+    elif filters[path] == "owner": #quase igual ao que fizemos juntos, mas adicionei uns passos a mais quando se trata de nome próprios
+        status = query_filter #recebendo o param cru caso os nomes sejam case sensitive
+        if "list" in querystring_parameters.keys():
+            list_mode = querystring_parameters["list"] == "True"
+            queryset = (
+                ToDoList.objects(status=status).all()
+                if list_mode
+                else ToDo.objects(status=status).all()
+            )
+            if not queryset: #se a lista retornar vazia, tentar com o nome todo lowercase
+                status = query_filter.lower()
+                queryset = (
+                ToDoList.objects(status=status).all()
+                if list_mode
+                else ToDo.objects(status=status).all()
+            )
+        else:
+            queryset = (
+                list(ToDoList.objects(status=status).all())
+                + list(ToDo.objects(status=status).all())
+            )
+            if not queryset: #mesma coisa aqui
+                status = query_filter.lower()
+                queryset = (
+                list(ToDoList.objects(status=status).all())
+                + list(ToDo.objects(status=status).all())
+                )
 
     elif filters[path] == "contribuitor":
         queryset = ToDoList.objects(owner=query_filter).all()
